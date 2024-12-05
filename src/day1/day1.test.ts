ebb8aff30b6704cc3f4de2e993eec28d0c582048
Holdyy <alexdtholdsworth@gmail.com>
2024-12-05 19:51:57 +0000

    deleting last year

--- src/day1/day1.test.ts
+++ src/day1/day1.test.ts
@@ -1,13 +1,12 @@
-import { logAnswer } from '../utils/logging';
 import { day1 } from './day1';
-import { data, testData } from './day1.data';
+import { logAnswer } from '../utils/logging';
 
 test('Provided test cases', () => {
-  expect(day1(testData)).toBe(142);
+  expect(day1()).toBe(1);
 });
 
 test('Returns an answer', () => {
-  logAnswer(day1(data));
-  expect(typeof day1(data)).toBe('number');
-  expect(day1(data)).toBeGreaterThan(0);
+  logAnswer(day1());
+  expect(typeof day1()).toBe('number');
+  expect(day1()).toBeGreaterThan(0);
 });
--- src/day1/day1.ts
+++ src/day1/day1.ts
@@ -1,17 +1,3 @@
-export const day1 = (data: string) => {
-  let numbers = "one, two, three, four, five, six, seven, eight, nine"
-  let NumberArray = []
-  let Round = []
-  let Number = 0
-  let score = 0
-
-  for (let i = 0; i < data.length; i++) {
-    NumberArray = data[i].match(/\d/g);
-
-    Round = NumberArray[0] + NumberArray[NumberArray.length - 1]
-    Number = +Round
-    score += Number
-
-  }
-  return score;
+export const day1 = () => {
+  return 1;
 };
